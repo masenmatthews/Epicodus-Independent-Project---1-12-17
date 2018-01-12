153a80a1eae979c6cf91ad4c69877c6b44112760
Masen Matthews <masenmatthews@gmail.com>
2018-01-12 10:34:56 -0800

    work on implementing JavaScript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $("#main form").submit(function() {
+  $("#main form").submit(function(event) {
+    var questionone = parseInt($("#questionone").val();
+    var questiontwo = parseInt($("#questiontwo").val();
+    var questionthree = $("#questionthree").val();
     var questionfour = $("#questionfour").val();
     var questionfive = $("#questionfive").val();
-  }
+
+    if(questionthree === "blank") {
+        alert("Please respond to all questions!");
+    }
 
 
+    event.preventDefault();
   });
 });
