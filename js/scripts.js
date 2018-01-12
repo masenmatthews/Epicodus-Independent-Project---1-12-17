34b77135ea1cf84635b519ffb0b6c73eb51f5b94
Masen Matthews <masenmatthews@gmail.com>
2018-01-12 11:05:33 -0800

    implement JavaScript functionality into survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-  $("#main form").submit(function(event) {
-    var questionone = parseInt($("#questionone").val();
-    var questiontwo = parseInt($("#questiontwo").val();
+  $("#main").submit(function(event) {
+    var questionone = parseInt($("#questionone").val());
+    var questiontwo = parseInt($("#questiontwo").val());
     var questionthree = $("#questionthree").val();
     var questionfour = $("#questionfour").val();
     var questionfive = $("#questionfive").val();
 
-    if(questionthree === "blank") {
+    if(questionone <3 && questiontwo <30 && questionthree === "no" || questionone === "startup" || questionfive === "vegan") {
+      $("#ruby").show();
+    } else if (questionone >5 && questiontwo >30 && questionthree === "yes") {
+      $("#java").show();
+    } else if (questionone >= 4 && questiontwo >= 25 && questionthree === "no" || questionfour === "enterprise" || questionfive === "microsoft") {
+      $("#csharp").show();
+    } else if (questionthree === "blank" || questionfour === "blank" || questionfive ==="blank") {
         alert("Please respond to all questions!");
     }
-
-
     event.preventDefault();
   });
 });
