332e024fe7cdf963998b1f94e201e4d08ab06960
Masen Matthews <masenmatthews@gmail.com>
2018-01-12 11:53:53 -0800

    improve survey results, tweak JavaScript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     var questionfour = $("#questionfour").val();
     var questionfive = $("#questionfive").val();
 
-    if(questionone <3 && questiontwo <30 && questionthree === "no" || questionone === "startup" || questionfive === "vegan") {
+    if(questionone <3 && questiontwo <27 && questionthree === "no") {
       $("#ruby").show();
-    } else if (questionone >5 && questiontwo >30 && questionthree === "yes" || questionfour === "enterprise") || (questionfive === "mobile") {
+    } else if (questionone >5 && questiontwo >30 && questionthree === "yes") {
       $("#java").show();
-    } else if (questionone >= 4 && questiontwo >= 25 && questionthree === "no" || questionfour === "enterprise" || questionfive === "microsoft") {
+    } else if (questionone === 4 && questiontwo === 25 && questionthree === "no") {
       $("#csharp").show();
     } else if (questionthree === "blank" || questionfour === "blank" || questionfive ==="blank") {
         alert("Please respond to all questions!");
