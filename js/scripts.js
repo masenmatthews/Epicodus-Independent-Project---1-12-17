5f63916b4e71ad5949ef4e9ce42f610be63b8d3e
Masen Matthews <masenmatthews@gmail.com>
2018-01-12 09:31:12 -0800

    make survey questions, add submit button

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#main form").submit(function() {
+    var questionfour = $("#questionfour").val();
+    var questionfive = $("#questionfive").val();
+  }
+
+
+  });
+});
